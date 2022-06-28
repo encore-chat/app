afa52b1c2e042088050fbc0ebfda78bd103f125a
vcokltfre <vcokltfre@gmail.com>
2022-06-28 17:49:20 +0100

    feat: initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<svelte:head>
+  <title>Encore - Touch grass.</title>
+</svelte:head>
+
+<div class="main">
+  <img src="/encore.svg" alt="Encore logo" />
+  <h1>Encore</h1>
+  <h2>Touch grass.</h2>
+</div>
+
+<style lang="scss">
+  .main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+
+    img {
+      height: 20%;
+    }
+
+    h1 {
+      font-size: 4em;
+    }
+
+    h2 {
+      font-size: 2em;
+      color: #f6f6f6aa;
+    }
+
+    h1,
+    h2 {
+      padding: 0;
+      margin: 0;
+    }
+  }
+</style>
